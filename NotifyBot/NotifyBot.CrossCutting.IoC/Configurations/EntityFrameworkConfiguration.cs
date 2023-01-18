ebdb37c35794eb9f71426a9c6c3e5b90a381b6a2
Marccus Zavadzki <marcuszavadzki72@gmail.com>
2023-01-18 11:58:00 -0300

    add views e identity context on infra

--- NotifyBot/NotifyBot.CrossCutting.IoC/Configurations/EntityFrameworkConfiguration.cs
+++ NotifyBot/NotifyBot.CrossCutting.IoC/Configurations/EntityFrameworkConfiguration.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using NotifyBot.Data.MySql;
@@ -10,10 +11,17 @@ namespace NotifyBot.CrossCutting.IoC.Configurations
         public static void AddEntityFrameworkConfiguration(this IServiceCollection services, IConfiguration configuration)
         {
             var connectionString = configuration["SQL_CONNECTION"];
+            var connectionStringIdentity = configuration["SQL_CONNECTION_IDENTITY"];
 
             services.AddDbContext<ApplicationDbContext>(options => {
                 options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)).UseSnakeCaseNamingConvention();
             }, ServiceLifetime.Scoped);
+
+            services.AddDbContext<IdentityContext>(options => {
+                options.UseMySql(connectionStringIdentity, ServerVersion.AutoDetect(connectionStringIdentity));
+            });
+
+            services.AddIdentityCore<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false).AddEntityFrameworkStores<ApplicationDbContext>();
         }
     }
 }
--- NotifyBot/NotifyBot.Web/wwwroot/js/site.js
+++ NotifyBot/NotifyBot.Web/wwwroot/js/site.js
@@ -1,4 +1,12 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿$(document).ready(function () {
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+    $('.scrollspy').scrollSpy();
+    $('.tooltipped').tooltip();
 
-// Write your JavaScript code.
+    $('#back-to-top').click(function () {
+        $("html, body").animate({
+            scrollTop: 0
+        }, 700);
+    });
+});
